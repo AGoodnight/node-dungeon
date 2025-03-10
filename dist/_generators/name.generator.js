156622f665b770e4764d009f3cf11e19d915395e
Adam Goodnight <adamgoodnight@Adams-MBP.attlocal.net>
2025-03-10 15:38:43 -0400

    removed ignored items

--- dist/_generators/name.generator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateName = generateName;
-function generateName(seed) {
-    const names = ["Goblin", "Orc", "Elf", "Human", "Dragon"];
-    const index = seed % names.length;
-    return names[index];
-}
