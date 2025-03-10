6f24abdc159318dac4fa8d51f6a44acd538e8628
Adam Goodnight <adamgoodnight@Adams-MBP.attlocal.net>
2025-03-10 15:39:29 -0400

    removed m00t

--- src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var chalk_1 = require("chalk");
-var app = (0, express_1.default)();
-app.listen(8080, function () {
-    console.log(chalk_1.default.green("Running Server on localhost:8080"));
-});
-exports.default = app;
